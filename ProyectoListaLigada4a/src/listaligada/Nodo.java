dc64d43c2bb43ef5f227b139d4a5924a654c2e80
Emmafloh13 <112911915+Emmafloh13@users.noreply.github.com>
2022-11-10 11:33:10 -0600

    Primer commit

---
+++ ProyectoListaLigada4a/src/listaligada/Nodo.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listaligada;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Nodo<T> {
+    private T dato; //dato almacenado
+    private Nodo siguiente;//apuntador al siguente nodo
+    
+    public Nodo(T d){
+        this.dato=d;
+        this.siguiente=null;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo otroNodo) {
+        this.siguiente = otroNodo;
+    }
+    
+}
+ 
---
+++ ProyectoListaLigada4a/src/listaligada/TestingLigadaLigada.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listaligada;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class TestingLigadaLigada {
+    
+    public static void main (String[] args) throws IOException{
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+        String entrada;
+        int dato;
+        char respuesta;
+        
+        ListaLigada lista=new ListaLigada();
+        
+        while (true){ //Ciclo infinito
+        System.out.println("Escribe un dato entero:");
+        entrada=bufer.readLine();
+        dato=Integer.parseInt(entrada);
+        lista.add(dato);
+            System.out.println("Se sgrego el dato a la lista");
+            System.out.println("¿Quieres agregar mas datos? (s/n:)");
+            entrada=bufer.readLine();
+            respuesta=entrada.charAt(0);
+            if(respuesta=='n' || respuesta=='N')
+            break;
+        }
+        
+        System.out.println("-------------------------");
+        System.out.println("Contenido de la lista ligada: ");
+        lista.printList();
+        /*
+        System.out.println("---------------------------");
+        System.out.println("Insercion de nuevo nodo");
+        System.out.println("Escribe el elemento a insertar: ");
+        entrada = bufer.readLine();
+        dato = Integer.parseInt(entrada);
+        System.out.println("Escriba la posicion en donde quieres insertar: ");
+        entrada = bufer.readLine();
+        int p=Integer.parseInt(entrada);
+        lista.add(dato, p);
+        
+        System.out.println("------------------------------");
+        System.out.println("Contenido de la lista ligada: ");
+        lista.printList();
+*/
+        
+    }
+}
